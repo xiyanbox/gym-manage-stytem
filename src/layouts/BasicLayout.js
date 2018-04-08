e3f0ddbbb063e229fb291029a0e9dc0d45136f85
vlong <13735800742@163.com>
2018-04-08 20:00:40 +0800

    修改系统名称

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -111,9 +111,9 @@ class BasicLayout extends React.PureComponent {
   getPageTitle() {
     const { routerData, location } = this.props;
     const { pathname } = location;
-    let title = 'Ant Design Pro';
+    let title = 'Gym Manage Stytem';
     if (routerData[pathname] && routerData[pathname].name) {
-      title = `${routerData[pathname].name} - Ant Design Pro`;
+      title = `${routerData[pathname].name} - Gym Manage Stytem`;
     }
     return title;
   }
